b14d0879262bfd715ed429f79e51358209e29df3
Denis Sidelkovsky <dsidelkovsky@gmail.com>
2024-01-18 06:12:40 +0300

    Implemented the main page and wrote readme.md.

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
-# Проект-плейсхолдер для интенсива React
+# D&D Monsters
 
-Этот проект создан с помощью Create React App (с поддержкой TypeScript), и в него добавлены следующие библиотеки:
+This web app shows basic information about monsters from the fantasy tabletop role-playing game "Dungeons and Dragons" (5th edition). It fetches data from a [free API](https://www.dnd5eapi.co/docs/#overview). Currently this API provides images for only a small subset of monsters, so I'm showing a placeholer image in cases where a proper image is not available.
 
-- React Router
-- Redux Toolkit
-- Material Design
+This is a practice project. My main purpose was to learn the Material UI library which I had never used before.
 
-На данный момент реализована только страница регистрации (и связанная с ней логика, в том числе валидация формы). Данные пока не сохраняются в Local Storage.
+## App features:
 
-Я до сих пор не выбрал тематику сайта и API, которое он будет использовать. Хочу сначала доделать sign up / sign in.
+- Sign up and Sign in
+- Search monsters by name
+- Check out individual monster pages with stats
+- Browse Search History when signed in
+- Add monsters to Favorites when signed in
+- User data is saved to localStorage and is restored upon revisiting the page
+
+## Technologies used:
+
+- TypeScript programming language
+- React library for rendering
+- React Router library for routing
+- Material UI component library
+- Redux library for state management
+- dayjs library for timestamps on the Search History page
+
+Please check out the [live version](https://dnd5e-monsters.netlify.app/) of this app!
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,11 +1,132 @@
 import MonsterSearchBar from "widgets/monster-search-bar";
-import PlaceholderPage from "shared/ui/placeholder";
+import { Paper, Stack, Typography, Link, Button, Box } from "@mui/material";
+import { ReactComponent as Logo } from "shared/ui/logo.svg";
+import { useNavigate } from "react-router-dom";
+import URL_PATHS from "app/url-paths";
+import { QUERY_THAT_RETURNS_ALL_ITEMS } from "features/search";
+import useMediaQuery from "@mui/material/useMediaQuery";
+
+const biggerScreenBreakpoint = 1200;
+
+const reversedLogoStyle = {
+  transform: "scaleX(-1)",
+};
+
+const leftAndRightBoxStyle = {
+  padding: 4,
+  width: "20em",
+  flexShrink: 0,
+};
+
+const midBoxStyle = {
+  padding: 1,
+  flexGrow: 1,
+  flexShrink: 1,
+};
 
 const MainPage = () => {
+  const navigate = useNavigate();
+  const biggerScreen = useMediaQuery(`(min-width:${biggerScreenBreakpoint}px)`);
+
+  const handleButtonClick = () => {
+    navigate(URL_PATHS.searchRoot + "/" + QUERY_THAT_RETURNS_ALL_ITEMS);
+  };
+
   return (
     <>
       <MonsterSearchBar />
-      <PlaceholderPage pageName="Main" />
+      <Paper sx={{ display: "flex", marginBottom: "1em" }}>
+        {biggerScreen && (
+          <Box sx={leftAndRightBoxStyle}>
+            <Logo style={reversedLogoStyle} />
+          </Box>
+        )}
+        <Box sx={midBoxStyle}>
+          <Typography variant="h2" textAlign={"center"}>
+            D&D Monsters
+          </Typography>
+          <Stack spacing={2}>
+            <Typography>
+              This web app shows basic information about monsters from the
+              fantasy tabletop role-playing game "Dungeons and Dragons" (5th
+              edition). It fetches data from a{" "}
+              <Link href="https://www.dnd5eapi.co/docs/#overview">
+                free API
+              </Link>
+              . Currently this API provides images for only a small subset of
+              monsters, so I'm showing a placeholer image in cases where a
+              proper image is not available.
+            </Typography>
+            <Typography>
+              This is a practice project. My main purpose was to learn the
+              Material UI library which I had never used before.
+            </Typography>
+            <Box>
+              <Typography variant="h3" fontSize={"1.2em"}>
+                App features:
+              </Typography>
+              <Typography component="ul">
+                <li>
+                  <strong>Sign up</strong> and <strong>Sign in</strong>
+                </li>
+                <li>
+                  <strong>Search</strong> monsters by name
+                </li>
+                <li>
+                  Check out <strong>individual monster pages</strong> with stats
+                </li>
+                <li>
+                  Browse <strong>Search History</strong> when signed in
+                </li>
+                <li>
+                  Add monsters to <strong>Favorites</strong> when signed in
+                </li>
+                <li>
+                  User data is saved to localStorage and is restored upon
+                  revisiting the page
+                </li>
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h3" fontSize={"1.2em"}>
+                Technologies used:
+              </Typography>
+              <Typography component="ul">
+                <li>
+                  <strong>TypeScript</strong> programming language
+                </li>
+                <li>
+                  <strong>React</strong> library for rendering
+                </li>
+                <li>
+                  <strong>React Router</strong> library for routing
+                </li>
+                <li>
+                  <strong>Material UI</strong> component library
+                </li>
+                <li>
+                  <strong>Redux</strong> library for state management
+                </li>
+                <li>
+                  <strong>dayjs</strong> library for timestamps on the Search
+                  History page
+                </li>
+              </Typography>
+            </Box>
+            <Typography>
+              Thank you for checking out this app. Have fun!
+            </Typography>
+            <Button onClick={handleButtonClick} sx={{ alignSelf: "center" }}>
+              Show All Monsters
+            </Button>
+          </Stack>
+        </Box>
+        {biggerScreen && (
+          <Box sx={leftAndRightBoxStyle}>
+            <Logo />
+          </Box>
+        )}
+      </Paper>
     </>
   );
 };
